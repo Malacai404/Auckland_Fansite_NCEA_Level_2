777fb92d33f1934dbe6a3c4d508f1727d5435284
Malacai404 <malacai404@gmail.com>
2025-03-12 11:41:39 +1300

    implemented flask - Flask Task 2

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print("Hello World!")
-print("Hello Mr Schetler!")
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name = "Shay"
+    return render_template("index.html", name=name)
+@app.route('/admin')
+def admin():
+    return "<h1>Hello Admin!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
